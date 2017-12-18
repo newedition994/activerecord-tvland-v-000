36fbdbea26059eb70cb3b8c53640d74a6794096e
Breyanna Vass <breyanna.bvass@gmail.com>
2017-12-18 21:48:22 +0000

    automatically backed up by learn

--- db/migrate/006_add_catchphrase_column_to_characters.rb
+++ db/migrate/006_add_catchphrase_column_to_characters.rb
@@ -1,6 +1,6 @@
 class AddCatchphraseColumnToCharacters < ActiveRecord:: Migration
   def change
-    add_column :characters, :catchphrase, :string
+    add_column :characters, :actor_id, :integer
   end
 
 end
