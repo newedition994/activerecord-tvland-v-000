f8ac2846843cc718e21cf99f4100083d098c002c
Breyanna Vass <breyanna.bvass@gmail.com>
2017-12-18 21:51:16 +0000

    automatically backed up by learn

--- db/migrate/20171218213057_add_foreign_id_columns_to_shows.rb.rb
+++ db/migrate/20171218213057_add_foreign_id_columns_to_shows.rb.rb
@@ -1,4 +1,4 @@
-class AddForeignIdColumnsToShows.rb < ActiveRecord::Migration
+class AddForeignIdColumnsToShows < ActiveRecord::Migration
   def change
     add_column :shows, :character_id, :integer
   end
